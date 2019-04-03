d052d3c5990c641d509def8628763d561e08e364
liukai <liukai8424@126.com>
2019-04-03 09:40:31 +0800

    commit

---
+++ component-test/src/main/java/com/xrds/basic/test/common/CommonTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ *	坤普
+ * Copyright (c) 2016-2019 KunPu,Inc.All Rights Reserved.
+ */
+package com.xrds.basic.test.common;
+
+/**
+ * 
+ * @author GSZY
+ * @version $Id: CommonTest.java, v 0.1 2019年4月3日 上午9:29:43 GSZY Exp $
+ */
+public class CommonTest {
+
+  /**
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+
+  }
+
+}
